3fba2cccb066c380e6afb99389c7b1127ab1d97c
Akir0s <akiroshato@gmail.com>
2021-03-29 05:30:09 +0300

    1hmera2

--- 42projects/C02/ex12/ft_print_memory.c
+++ 42projects/C02/ex12/ft_print_memory.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void ft_print_address(char *a)
+void    ft_print_address(char *a)
 {   
-    char *b= &*a ;
-    char hex[] = "01234567890abcdef";
-    
-    printf("%p\n", b);
+    char    addr[16];
+    long    c_addr;
+    int     i;
 
+    i = 0;
+    c_addr = (long)a;
+    while (c_addr > 0)
+    {
+        addr[i] = c_addr % 16;
+        if (addr[i] < 10)
+            addr[i] += '0';
+        else
+            addr[i] += 87;
+        c_addr /= 16 ;
+        i++;
+    }
+    while (i < 16)
+    {
+        addr[i] = '0';
+		i++;
+    }
+    while (i >= 0)
+    {
+        write(1, &addr[i], 1);
+        i--;
+    }
+    write(1, ":\n", 2);
 }
 
-int main()
+int     main()
 {
-    char A[] = "hello It's me again.    ";
-    ft_print_address(A);
+    char A[] = "hello It's me again.You know i can't forget you miss dunno why";
     printf("test address : %p\n", A);
-
+    ft_print_address(A);
 }
